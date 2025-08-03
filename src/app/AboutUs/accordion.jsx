f132a37a49db0a3246b5695fe28264a02d8423e1
Loris Jared Ndonga <130836683+ndongaloris@users.noreply.github.com>
2025-08-03 09:58:45 +0000

    back to how it was

--- src/app/AboutUs/accordion.jsx
+++ src/app/AboutUs/accordion.jsx
@@ -1,20 +1,20 @@
 "use client";
 
-// import Accordion from "react-bootstrap/Accordion";
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import Accordion from "react-bootstrap/Accordion";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-// export default function Dropdown({keyProp, question, answer}){
-//   return (
-//     // <Accordion>
-//     //   <Accordion.Item eventKey={keyProp.toString()}>
-//     //     <Accordion.Header>
-//     //       {question}
-//     //     </Accordion.Header>
-//     //     <Accordion.Body>
-//     //       {answer}
-//     //     </Accordion.Body>
-//     //   </Accordion.Item>
-//     // </Accordion>
-//   );
-// }
+export default function Dropdown({keyProp, question, answer}){
+  return (
+    <Accordion>
+      <Accordion.Item eventKey={keyProp.toString()}>
+        <Accordion.Header>
+          {question}
+        </Accordion.Header>
+        <Accordion.Body>
+          {answer}
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
