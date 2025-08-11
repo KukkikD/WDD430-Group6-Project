5921ed3a9a3bfa573854b68c559c25456d1c6f75
Loris Jared Ndonga <130836683+ndongaloris@users.noreply.github.com>
2025-08-11 18:58:51 +0300

    progress

--- src/app/AboutUs/page.tsx
+++ src/app/AboutUs/page.tsx
@@ -5,12 +5,12 @@ import OurTeam from "./ourTeam"
 
 export default function page(){
     return (       
-            <>
+            <div>
                 <h1 className="text-5xl text-[#b8aa8d] mt-2 mb-2">About Us</h1>
                 <OurStory />
                 <OurTeam />
                 <OurMission/>
                 <FAQ />
-            </>
+            </div>
         )
 }
