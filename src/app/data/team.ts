28ee131267346f8098be4bf83cd4d0f1ee309e71
Loris Jared Ndonga <130836683+ndongaloris@users.noreply.github.com>
2025-08-12 22:28:24 +0300

    .

--- src/app/data/team.ts
+++ src/app/data/team.ts
@@ -1,30 +1,30 @@
 export const Team = [
     {
-        image : "",
+        image : "https://media.licdn.com/dms/image/v2/D5603AQEtLI9m8Yg_Wg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1691321176582?e=1758153600&v=beta&t=BkuovuVymi9lQ-vaC1LFOnRXbPnfugxjEq84o8oC3_w",
         fullName:"Loris Jared Ndonga",
         projectRole:"Software Developer",
         linkedIn : "https://linkedin.com/in/lorisjaredndonga",
     },
     {
-        image : "",
+        image : "https://media.licdn.com/dms/image/v2/C4E03AQHQU2k6Dx8fFQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1659363354502?e=1758153600&v=beta&t=43e6hrPj8-6XMZYHzTfZceHsY5LTtGd3McW493bjfvQ",
         fullName:"Amornrat Dizon",
         projectRole:"Project Manager / Designer",
         linkedIn : "https://th.linkedin.com/in/amornratdizon"
     },
     {
-        image : "",
+        image : "https://media.licdn.com/dms/image/v2/D4D03AQHQtadd7SB0Zw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1711907610266?e=1758153600&v=beta&t=l2H1QZ90Nj2TBmWVts99505CujRqbSrE20Tn8qz2ZbI",
         fullName:"Andrew O. Mogbeyiromore",
         projectRole:"Back-End Developer",
         linkedIn : "https://www.linkedin.com/in/andrew-mogbeyiromore/"
     },
     {
-        image : "",
+        image : "https://media.licdn.com/dms/image/v2/D5603AQE3BedkBo69zA/profile-displayphoto-crop_800_800/B56ZiMqiBfG0AI-/0/1754706617708?e=1758153600&v=beta&t=HsRDC-XzgNWwec5UPrvDOKiQEmEKIP900t2L3hz_QM8",
         fullName:"Tanya Glea Ann Trampp",
         projectRole:"Front-End Developer",
         linkedIn : "https://www.linkedin.com/in/ttrampp"
     },
     {
-        image : "",
+        image : "https://media.licdn.com/dms/image/v2/D4D03AQFdwLd2Sa07wA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1702064706162?e=1758153600&v=beta&t=-Wyf3bDQjXlhoQo9bnqDK5ZxelF8JLRcirKiH_zgTNQ",
         fullName:"Jordan Erick Larcher",
         projectRole:"Software Developer",
         linkedIn : "https://www.linkedin.com/in/jordan-erick-larcher/"
