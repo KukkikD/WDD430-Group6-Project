3d2846673c109320dc0b427157e38b3f5e11e32f
KukkikD <adeezon9@gmail.com>
2025-08-12 22:27:17 -0700

    fix register / random seller spotlight

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,14 +1,22 @@
-import { DefaultSession, DefaultUser } from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
+  interface User {
+    id: string;
+    role: string;
+  }
+
   interface Session {
     user: {
       id: string;
       role: string;
     } & DefaultSession["user"];
   }
+}
 
-  interface User extends DefaultUser {
-    role: string;
+declare module "next-auth/jwt" {
+  interface JWT {
+    id?: string;
+    role?: string;
   }
 }
